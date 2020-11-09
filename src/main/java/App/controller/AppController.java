5932b7221065be61f9146ee9bda2ab8d1d626474
devivasha <nat_ivaniv@ukr.net>
2020-11-09 18:16:14 +0200

    Initial commit

---
+++ src/main/java/App/controller/AppController.java
@@ -0,0 +1,12 @@
+package App.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+    @GetMapping("/home")
+    public String hello() {
+        return "Hello World";
+    }
+}
